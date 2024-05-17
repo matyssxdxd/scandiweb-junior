1b35b5a69a267dbfe9dcb5ef9092b8b849af825b
matyssxd <matissp1302@gmail.com>
2024-05-17 20:32:47 +0300

    updated from http to https

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
     uri: 'https://french-locations.000webhostapp.com/graphql',
-    cache: new InMemoryCache(),
+    fetchOptions: { mode: 'no-cors' },
+  cache: new InMemoryCache(),
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
